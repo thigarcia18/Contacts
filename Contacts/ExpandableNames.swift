dc00abb41cd7fb5d1a504948d2350b89cd0038f1
Thiago Garcia <tgalves18@gmail.com>
2018-04-11 00:40:52 -0300

    Implemented expandable sections

---
+++ Contacts/ExpandableNames.swift
@@ -0,0 +1,16 @@
+//
+//  ExpandableNames.swift
+//  Contacts
+//
+//  Created by Thiago Garcia on 11/04/18.
+//  Copyright © 2018 iGarcia. All rights reserved.
+//
+
+import Foundation
+
+struct ExpandableNames {
+    
+    var isExpanded: Bool
+    let names: [String]
+    
+}
--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -11,24 +11,12 @@ import UIKit
 class ViewController: UITableViewController {
     
     let cellId = "cellId"
-    
-    let names = [
-        "Amy", "Bill", "Zack", "Steve", "Jack", "Jill", "Mary"
-    ]
-    
-    let cNames = [
-        "Carl", "Chris", "Christina", "Cameron"
-    ]
-    
-    let dNames = [
-        "David", "Dan"
-    ]
-    
-    let twoDimensionalArray = [
-        ["Amy", "Bill", "Zack", "Steve", "Jack", "Jill", "Mary"],
-        ["Carl", "Chris", "Christina", "Cameron"],
-        ["David", "Dan"],
-        ["Patrick", "Patty"]
+
+    var twoDimensionalArray = [
+        ExpandableNames(isExpanded: true, names: ["Amy", "Bill", "Zack", "Steve", "Jack", "Jill", "Mary"]),
+        ExpandableNames(isExpanded: true, names: ["Carl", "Chris", "Christina", "Cameron"]),
+        ExpandableNames(isExpanded: true, names: ["David", "Dan"]),
+        ExpandableNames(isExpanded: true, names: ["Patrick", "Patty"])
     ]
     
     var showIndexPaths = false
@@ -37,7 +25,7 @@ class ViewController: UITableViewController {
         var indexPathsToReload = [IndexPath]()
         
         for section in twoDimensionalArray.indices {
-            for row in twoDimensionalArray[section].indices {
+            for row in twoDimensionalArray[section].names.indices {
                 let indexPath = IndexPath(row: row, section: section)
                 indexPathsToReload.append(indexPath)
             }
@@ -62,10 +50,42 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let label = UILabel()
-        label.text = "Header"
-        label.backgroundColor = UIColor.lightGray
-        return label
+        let button = UIButton(type: .system)
+        button.setTitle("Close", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .yellow
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        button.addTarget(self, action: #selector(handleExpandClose), for: .touchUpInside)
+        
+        button.tag = section
+        
+        return button
+    }
+    
+    @objc func handleExpandClose(button: UIButton) {
+        let section = button.tag
+        
+        var indexPaths = [IndexPath]()
+        for row in twoDimensionalArray[section].names.indices {
+            let indexPath = IndexPath(row: row, section: section)
+            indexPaths.append(indexPath)
+        }
+        
+        let isExpanded = twoDimensionalArray[section].isExpanded
+        twoDimensionalArray[section].isExpanded = !isExpanded
+        
+        button.setTitle(isExpanded ? "Open" : "Close", for: .normal)
+        
+        if isExpanded {
+            tableView.deleteRows(at: indexPaths, with: .fade)
+        } else {
+            tableView.insertRows(at: indexPaths, with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -73,12 +93,16 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return twoDimensionalArray[section].count
+        if !twoDimensionalArray[section].isExpanded {
+            return 0
+        }
+        
+        return twoDimensionalArray[section].names.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        let name = twoDimensionalArray[indexPath.section][indexPath.row]
+        let name = twoDimensionalArray[indexPath.section].names[indexPath.row]
         
         cell.textLabel?.text = name
         
